8d1e3cd79f1bc160e58813a9ca031723f66465eb
meta-crawler <williamlei0906@gmail.com>
2023-01-13 11:58:32 -0500

    complete except for carousel part

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import TopBar from "components/TopBar";
 import RoadMap from "components/RoadMap";
+import AboutDecentraland from "components/AboutDecentraland";
+import BlogContactUs from "components/BlogContactUs";
 
 const Views = () => {
   return (
     <>
       <TopBar />
       <RoadMap />
+      <AboutDecentraland />
+      <BlogContactUs />
     </>
   );
 };
